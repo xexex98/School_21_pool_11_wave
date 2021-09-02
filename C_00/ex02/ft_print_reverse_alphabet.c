53a78796c947e9f8779d3a3d4804a67aca7942c9
xexex98 <89549736+xexex98@users.noreply.github.com>
2021-09-02 22:43:14 +0300

    Add files via upload

---
+++ C_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbarra <mbarra@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/19 19:50:55 by mbarra            #+#    #+#             */
+/*   Updated: 2021/08/19 22:26:37 by mbarra           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	za;
+
+	za = 'z';
+	while (za >= 'a')
+	{
+		ft_putchar(za);
+		za--;
+	}
+}
---
+++ C_00/ex04/main.c
@@ -0,0 +1,6 @@
+void	ft_is_negative(int i);
+int	main(void)
+{
+	ft_is_negative(0);
+	return (0);
+}
